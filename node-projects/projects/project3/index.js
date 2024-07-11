e9add60e2a0fb7a59bc6f451795701ac092f01c3
Ubuntu <labuser@ubuntu2204.sf1kp2lqq1aujokso0n5huq4ib.rx.internal.cloudapp.net>
2024-07-11 08:48:39 +0000

    Updated Server Backend

--- node-projects/projects/project3/index.js
+++ node-projects/projects/project3/index.js
@@ -25,21 +25,45 @@ server.on("error", () => console.log("Error while handling request"));
 // });
 
 server.on("request", (req, res) => {
+
+    const url = req.url;
+
     // Accessing request headers of incoming requests
     const{ headers } = req;
 
     // Getting user-agent value from request header
     const userAgent = headers['user-agent'];
 
-    // Setting the response header
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader('Date', new Date());
-    res.end(JSON.stringify({
-        "Platform": process.platform,
-        "UserAgent": userAgent,
-        "Date": new Date(),
-        "Message": "Hellos"
-    }));
+    if(url === '/textcontent'){
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Date', new Date());
+        res.end("Welcome to my App!!");
+    }
+
+    if(url === '/jsoncontent'){
+        // Setting the response header
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader('Date', new Date());
+        res.end(JSON.stringify({
+            "Platform": process.platform,
+            "UserAgent": userAgent,
+            "Date": new Date(),
+            "Message": "Hellos"
+        }));
+    }
+
+    if(url === '/htmlcontent'){
+        res.setHeader("Content-Type", "text/html");
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<h2>');
+        res.write('This is an HTML Response');
+        res.write('</h2>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
+    
 });
 
 server.listen(SERVER_PORT, SERVER_HOSTNAME, () => {
